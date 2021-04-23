4639d2ac664d922b6288a0d0ac0fd88918e2099d
Konrad Jazdzyk <konradsmkkd@gmail.com>
2021-04-23 10:56:07 +0200

    Widok_gry

---
+++ widok_gry/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Widok_gry from './components/widok_gry.js';
+
+
+
+
+ReactDOM.render(
+    <React.StrictMode>
+      <Widok_gry/>
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+  
