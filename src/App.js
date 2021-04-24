e9c6976394723951117617617bd0500a59a472ee
Konrad Jażdżyk <66627823+konradmkkd@users.noreply.github.com>
2021-04-24 16:48:35 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,58 @@
+import './App.css';
+import kartaImg from "./sample_karta.jpg"
+
+
+const score = 1234;
+const stopwatch = '12:34';
+
+function App() {
+ 
+
+  return (
+    <div className="App">
+    <div className="menu_top">
+    <header className="widok_gry_header">
+        <h3 class="klondike_title">Klondike</h3>
+      </header>
+      <div className="points_time">
+      <p className="score_points">Punkty: {score}</p>
+      <p className="score_time">Czas: {stopwatch}</p>
+      </div>
+      
+      </div>
+      <div className="karty_container">
+      <div className="karty_top">
+      <img src={kartaImg} alt="karta"></img>
+      <img id="druga_top" src={kartaImg} alt="karta"></img>
+      <img src={kartaImg} alt="karta"></img>
+      <img src={kartaImg} alt="karta"></img>
+      <img src={kartaImg} alt="karta"></img>
+      <img id="ostatnia_karta" src={kartaImg} alt="karta"></img>
+      </div>
+
+      <div className="karty_bottom">
+      <img src={kartaImg} alt="karta"></img>
+      <img src={kartaImg} alt="karta"></img>
+      <img src={kartaImg} alt="karta"></img>
+      <img src={kartaImg} alt="karta"></img>
+      <img src={kartaImg} alt="karta"></img>
+      <img src={kartaImg} alt="karta"></img>
+      <img id="ostatnia_karta" src={kartaImg} alt="karta"></img>
+      </div>
+      </div>
+      <div className="bottom_section">
+      <button className="undo_button">Cofnij</button>
+      <div className="info_section">
+      <p className="moves_left">Ilość ruchów do wykonania: 1234</p>
+      <p className="is_possible">Czy możliwe jest skończenie partii: TAK/NIE</p>
+      <p className="moves_done">Ruchy: 1234</p>
+
+      </div>
+      
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Widok_gry from './components/widok_gry.js';
+
+
+
+ReactDOM.render(
+    <React.StrictMode>
+      <Widok_gry/>
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+  
