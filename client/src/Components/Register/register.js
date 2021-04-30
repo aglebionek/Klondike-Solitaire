ad848b5dce4376c10413d71df9ca87a9db8039ce
Piotr Pośpiech <piootrp01@gmail.com>
2021-04-30 19:30:59 +0200

    Merge into login.js and register.js

--- client/src/Components/Register/register.js
+++ client/src/Components/Register/register.js
@@ -1,14 +1,40 @@
-import React from 'react';
-import Caption from "./Caption.js"
-import Form from "./Form.js"
-import Buttons from "./Buttons.js"
+import React,{useState} from 'react';
+import Axios from "axios";
 import "./register.css"
 function Register (){
+  const [usernameReg,setUsernameReg]=useState('')
+  const [passwordReg,setPasswordReg]=useState('')
+  const [emailReg,setEmailReg]=useState('')
+
+  const register = () => {
+    Axios.post('http://localhost:3001/register',{
+      username: usernameReg,
+      email: emailReg,
+      password: passwordReg,
+    }).then((response)=>{
+      console.log(response)
+    })
+  }
+
   return (
     <div>
-    <Caption />
-    <Form />
-    <Buttons />
+    <header className="header"> Rejestracja </header>
+    <div id="container">
+        <input type="text" placeholder="Nazwa użytkownika" onChange={(e)=>{
+            setUsernameReg(e.target.value);
+        }} />
+        <input type="email" placeholder="E-mail" onChange={(e)=>{
+            setEmailReg(e.target.value);
+        }}  />
+		
+        <input type="password" placeholder="Hasło" onChange={(e)=>{            
+            setPasswordReg(e.target.value);
+        }}  />
+    </div>
+    <div id="container">
+        <button type="button">Mam już konto</button>
+        <button type="button" onClick={register}>Zarejestruj się</button>
+    </div>
     </div>
   )
 }
--- client/src/Components/login/Login.js
+++ client/src/Components/login/Login.js
@@ -1,18 +1,51 @@
-import React from 'react';
-import Caption from "./Caption.js";
-import Form from "./Form.js";
-import Buttons from "./Buttons.js";
-import ForgetPassword from "./ForgetPassword.js";
+import React,{useState} from 'react';
+import Axios from "axios";
 import "./Login.css";
 
 
 function Login (){
+
+  const [username,setUsername]=useState('')
+  const [password,setPassword]=useState('')
+
+  const [loginStatus,setLoginStatus]=useState('')
+  const login =() =>{
+    Axios.post('http://localhost:3001/login',{
+      username: username,
+      password: password,
+    
+    }).then((response)=>{
+      if(response.data.message)
+      {
+        setLoginStatus(response.data.message);
+      }
+      else
+      {
+        setLoginStatus("Witaj "+response.data[0].username)
+      }
+    })
+  }
+
   return (
     <div>
-      <Caption />
-      <Form />
-      <Buttons />
-      <ForgetPassword />
+    <header className="header"> Logowanie </header>  
+    <div id="container">
+        <input type="text" placeholder="Nazwa użytkownika" onChange = {(e)=>{
+          setUsername(e.target.value);
+        }}          
+        />      
+		<input type="password" placeholder="Hasło" onChange = {(e)=>{
+          setPassword(e.target.value);
+        }} />
+    </div>
+    <div id="container">
+        <button type="button" className="button" onClick={login}>Zaloguj się</button>
+        <button type="button" className="button">Zarejestruj się</button>
+    </div>
+    <p>{loginStatus}</p>
+    <div id="container">
+        <p><a href="default.asp" target="_blank">Nie pamiętasz hasła?</a></p>
+    </div>
     </div>
   )
 }
