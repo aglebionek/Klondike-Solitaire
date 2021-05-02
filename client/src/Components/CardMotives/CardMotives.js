11c708e0053c27d0c279702a56acf8de40db2405
aglebionek <aglebionek@gmail.com>
2021-05-02 03:21:53 +0200

    Initial commit

---
+++ client/src/Components/CardMotives/CardMotives.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./CardMotives.css"
+
+function CardMotives() {
+  return (
+    <div className="card">
+        <span className="card__value"></span>
+        <span className="card__minisuit"></span>
+        <span className="card__mainsuit"></span>        
+    </div>
+  );
+}
+
+export default CardMotives;
