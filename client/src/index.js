08fc3747d1cd347719437a5e5112f3b4bfc26c2d
aglebionek <aglebionek@gmail.com>
2021-05-06 20:56:14 +0200

    tidy up

--- client/src/index.js
+++ client/src/index.js
@@ -4,12 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
-import CardMotives from './Components/CardMotives/CardMotives';
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <CardMotives />
+      <App />
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
