883a582d3fc5ebe9968f946afe36956f3e73762b
ruslav <mateusz.gajo9@wp.pl>
2021-05-11 16:05:44 +0200

    remove redundant app listener, body parser. resolve jest conflict. remove route to doesn't exist folder. rename query

--- client/src/App.js
+++ client/src/App.js
@@ -9,10 +9,9 @@ import Settings from "./Components/Settings";
 import GlobalStats from "./Components/GlobalStats/Stats";
 import GameView from "./Components/GameView/GameView";
 import LobbyMultiplayer from "./Components/Mutiplayer/LobbyMultiplayer";
-import CreateRoom from './Components/Mutiplayer/CreateRoom/CreateRoom';
-import JoinRoom from './Components/Mutiplayer/JoinRoom/JoinRoom';
+import CreateRoom from "./Components/Mutiplayer/CreateRoom/CreateRoom";
+import JoinRoom from "./Components/Mutiplayer/JoinRoom/JoinRoom";
 import Account from "./Components/Account/Account";
-import Test from "./Components/Test";
 
 function App() {
   return (
@@ -25,7 +24,6 @@ function App() {
       <Route path="/game-view" component={GameView} />
       <PrivateRoute path="/multiplayer" component={LobbyMultiplayer} />
       <PrivateRoute path="/account" component={Account} />
-      <PrivateRoute path="/test" component={Test} />
       <PrivateRoute path="/multiplayer/game-lobby" component={JoinRoom} />
       <PrivateRoute path="/multiplayer/create-room" component={CreateRoom} />
     </Switch>
