966ce663d290e85d0c0e5f99e5aa0a3b7ff06248
Piotr Pośpiech <piootrp01@gmail.com>
2021-05-14 19:45:51 +0200

    add timer and its methods:

--- client/src/Components/GameView/GameView.js
+++ client/src/Components/GameView/GameView.js
@@ -1,10 +1,72 @@
+import React from "react";
 import './GameView.css';
 
-function GameView() {
-  const score = 1234;
-  const stopwatch = '12:34';  
 
+
+class GameView extends React.Component {
+
+  state = {
+    timerOn: false,
+    timerStart: 0,
+    timerTime: 0
+  };
+  
+  moves = 0;
+  points = 0;
+  timeBonus = 1200;
+
+  
+
+  startTimer = () => {
+    this.setState({
+      timerOn: true,
+      timerTime: this.state.timerTime,
+      timerStart: Date.now() - this.state.timerTime
+    });
+    this.timer = setInterval(() => {
+      this.timeBonus--;
+      if(this.timeBonus < 0){
+        this.timeBonus = 0;
+      }
+      this.setState({
+        timerTime: Date.now() - this.state.timerStart        
+      });
+    }, 1000);
+
+  };
+
+  stopTimer = () => {
+    this.setState({ timerOn: false });
+    clearInterval(this.timer);
+    this.points=this.points+this.timeBonus;
+  };
+
+  componentDidMount(){
+    this.startTimer();
+  };
+
+  getBack() {
+    if(this.points - 10 > 0){
+    this.points = this.points - 10;
+    }
+    else
+    {
+    this.points = 0;
+    }
+  };
+
+  
+  
+  
+  render(){
+    const { timerTime } = this.state;
+    const score = this.points;
+    const moves = this.moves;
+   
+    let seconds = ("0" + (Math.floor(timerTime / 1000) % 60)).slice(-2);
+    let minutes = ("0" + (Math.floor(timerTime / 60000) % 60)).slice(-2);
   return (
+    
     <div className="App">
     <div className="menu_top">
         <header className="widok_gry_header">
@@ -12,7 +74,7 @@ function GameView() {
         </header>
         <div className="points_time">
         <p className="score_points">Punkty: {score}</p>
-        <p className="score_time">Czas: {stopwatch}</p>
+        <p className="score_time">Czas: {minutes} : {seconds}</p>
         </div>
 
         </div>
@@ -37,16 +99,18 @@ function GameView() {
         </div>
         </div>
         <div className="bottom_section">
-        <button className="undo_button">Cofnij</button>
+        <button className="undo_button" onClick={this.getBack} >Cofnij</button>
         <div className="info_section">
         <p className="moves_left">Ilość ruchów do wykonania: 1234</p>
         <p className="is_possible">Czy możliwe jest skończenie partii: TAK/NIE</p>
-        <p className="moves_done">Ruchy: 1234</p>
+        <p className="moves_done">Ruchy: {moves}</p>
 
       </div>
       </div>
     </div>
   );
-}
+  
+}}
+
 
 export default GameView;
