6ff238fcdc3da176bdb6e81d3a84abc40db6bdf0
Konrad Jazdzyk <konradsmkkd@gmail.com>
2021-05-18 10:29:45 +0200

    Add game end DRAW animation

---
+++ client/src/Components/GameView/game_end_animations/game_end_draw.js
@@ -0,0 +1,21 @@
+import React from "react";
+import styles from "./game_end_draw.css";
+
+
+function Game_end_draw(){
+
+return(
+
+<div className="draw_wrapper">
+
+<div class="draw-containter">
+<div href="#" class="draw_animation">
+        <span>REMIS</span>
+</div>
+</div>
+<div className="draw_blackout"></div>
+
+</div>
+)}
+
+export default Game_end_draw; 
