85c55af4e6d1f365864cf6b128f8fa4bd4257e24
RKędziora <kontakt.rkedziora@gmail.com>
2021-05-28 21:11:21 +0200

    update heroku 2/2

--- server/index.js
+++ server/index.js
@@ -3,14 +3,19 @@ const exampleRoute = require("./api/example/exampleRoute");
 const roomsRoute = require("./api/rooms/roomsRoute");
 const settingsRoute = require("./api/settings/settingsRoute");
 const statsRoute = require("./api/stats/statsRoute");
+const path = require("path");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+});
 app.get("/", (req, res) => {
   res.send("Some shit");
 });
 
+app.use(express.static(path.resolve(__dirname, '../client/build')));
 app.use("/example", exampleRoute);
 app.use("/rooms", roomsRoute);
 app.use("/settingsRoute", settingsRoute);
