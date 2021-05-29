9e24d5240763526281c8ece9edd008973f32a089
aglebionek <aglebionek@gmail.com>
2021-05-29 23:10:12 +0200

    Stworzeniu designu aplikacji Fixes #60

--- client/src/App.js
+++ client/src/App.js
@@ -1,239 +1,103 @@
-import React, { useState, useEffect } from "react";
-import "./Components/Account/AccountCyberpunk.css";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
+import "./App.css";
+import MainMenu from "./Components/MainMenu/MainMenu";
+import Login from "./Components/Login/Login";
+import Register from "./Components/Register/Register";
+import { Switch, Route, Redirect } from "react-router-dom";
+import Settings from "./Components/Settings";
+import GlobalStats from "./Components/GlobalStats/Stats";
+import GameView from "./Components/GameView/GameView";
+import LobbyMultiplayer from "./Components/Mutiplayer/LobbyMultiplayer";
+import CreateRoom from "./Components/Mutiplayer/CreateRoom/CreateRoom";
+import JoinRoom from "./Components/Mutiplayer/JoinRoom/JoinRoom";
+import Account from "./Components/Account/Account";
 
-const Account =() => {
-    const [show, setShow] = useState(false);
-    const [userName, setUserName] = useState('Nazwa użytkownika');
-    const [accountCreation, setAccountCreation] = useState('2020-01-01');
-    const [country, setCountry] = useState('Polska');
-    const [avatar, setAvatar] = useState('1');
-    const [temporaryAvatar, setTemporaryAvatar] = useState('avatar1');
-    const [newUsername, setNewUsername] = useState('');
-    const [newCountry, setNewCountry] = useState('');
-    
-    const [currentPassword, setCurrentPassword] = useState('admin');
-
-    const [oldPassword, setOldPassword] = useState('');
-    const [newPassword, setNewPassword] = useState('');
-    const [repeatPassword, setRepeatPassword] = useState(''); 
-    
-
-    const userId = 2;
-    const avatars =["avatar1", "avatar2","avatar3","avatar4","avatar5","avatar6"];
-
-    const nextAvatar = () => {
-        let index = avatars.indexOf(temporaryAvatar);
-        if (index === avatars.length - 1) index = 0;
-        else index++;
-        setTemporaryAvatar(avatars[index]);
-      };
-    
-      const previousAvatar = () => {
-        let index = avatars.indexOf(temporaryAvatar);
-        if (index === 0) index = avatars.length - 1;
-        else index--;
-        setTemporaryAvatar(avatars[index]);
-      };
-
-      const setNewData = () => {
-        const num = temporaryAvatar.match(/\d+/)[0];
-        setAvatar(Number(num));
-        if(newUsername != '') {
-            setUserName(newUsername);
-        } 
-        if(oldPassword===currentPassword && newPassword===repeatPassword){
-            setCurrentPassword(newPassword);
-        }else {
-        };
-        if(newCountry!=''){
-            setCountry(newCountry);
-        }
-        clearSettings();
-        
-      };
-
-      const clearSettings = () => {
-        setNewPassword('');
-        setOldPassword('');
-        setRepeatPassword('');
-        document.getElementsByName('editForm')[0].reset();
-        setShow(false);
-      }
-
-      
-      /*
-    useEffect(() => {
-        axios.get(`http://localhost:3000/account/${userId}`).then(({ data }) => {
-          const { username, registration_date, country, icon_id, password} = data.resp[0];     
-          setUserName(username);
-          setAccountCreation(registration_date);
-          setCountry(country);
-          setTemporaryAvatar("avatar"+icon_id);
-          setAvatar(icon_id) ;
-          setCurrentPassword(password);
-        });
-      }, []);
-      */
-      /*
-      <div className="profile-header-edit"> 
-                    <div className="edition-text">
-                        <button onClick={() => setShow(true)}>Edycja</button>
-                    </div>
-                </div>
-      */
-      const handleSubmit = (e) => {
-        e.preventDefault();
-        axios.put(`http://localhost:3000/account/edit/${userId}`, {
-        icon_id: avatar,
-        username: userName,
-        password: currentPassword,
-        country: country,
-        });
-      };
-    return (<>
-        <div className = "profile-container">
-          <div className='profile-menu-button-div'>
-            <a href="/" className='profile-menu-button'>
-              MENU
-            </a>
-          </div>
-            <header className="profile-heading">KONTO</header>
-            <div className="profile-header">
-                <div className="profile-avatar">
-                    <img draggable="false" className="account-avatar" src={`./images/avatar${avatar}.png`} alt="Awatar użytkownika" width="150" height="150" />
-                </div> 
-                <div className="profile-header-info"> 
-                    <div className="profile-username">{userName}</div>
-                    <div className="profile-creation-date">
-                        Profil utworzono: <span className = "profile-header-date">{accountCreation}</span>
-                    </div>
-                    <div className="profile-country-info">
-                        <div className="country-img">
-                            <span><img id="flag-picture"src="./images/polska.png" alt="Flag " width="25" height="20" /></span>
-                        </div>
-                        <div className="country-name">
-                            {country}
-                        </div>
-                        <div className="edition-text">
-                            <button className='profile-edit-button' onClick={() => setShow(true)}>Edycja</button>
-                        </div>
-                    </div>
-                </div>
-                
-                
-            </div>
-            <div className="profile-main">
-                <div className="profile-statistics"> 
-                    <div className="profile-statistics-inscription">Statystyki</div>
-                    <div className="profile-statistics-number-of-games stats">
-                        <div id="number-of-game-text">Gry</div>
-                        <div id="number-of-game-stats">146</div>
-                    </div>
-                    <div className="profile-statistics-number-of-points stats">
-                        <div id="number-of-points-text">Punkty</div>
-                        <div id="number-of-points-stats">23 590</div>
-                    </div>
-                    <div className="profile-statistics-W-L-D-games stats">
-                        <div id="W-L-D-games-text">W/P/R</div>
-                        <div id="W-L-D-games-stats">42/98/6</div>
-                    </div>
-                    <div className="profile-statistics-total-game-time stats">
-                        <div id="total-game-time-text">Całkowity czas gry</div>
-                        <div id="total-game-time-stats">7,3h</div>
-                    </div>
-                    <div className="profile-statistics-average-game-time stats">
-                        <div id="average-game-time-text">Średni czas gry</div>
-                        <div id="average-game-time-stats">2min 36sec</div>
-                    </div>
-                    <div className="profile-statistics-best-win-streak stats">
-                        <div id="best-win-streak-text">Winstreak</div>
-                        <div id="best-win-streak-stats">5</div>
-                    </div>
-                    <div className="profile-statistics-number-of-abandoned-games stats">
-                        <div id="number-of-abandoned-games-text">Liczba porzuconych gier</div>
-                        <div id="number-of-abandoned-games-stats">11</div>
-                    </div>
-                </div>
-            </div>
-        </div> 
-        <div style={show ? {display: 'flex'} : {display: 'none'}} className="background-modal">
-
-            <div className="modal-content">
-            <form onSubmit={handleSubmit} name="editForm">
-                <div className="modal-settings">Ustawienia</div>
-                <div className="modal-nick">
-                    <div className="row-one">Nazwa użytkownika</div>
-                        <input 
-                            className="modal-nick-input" 
-                            type="text" 
-                            onChange={(event) => {
-                                setNewUsername(event.target.value);
-                        }}
-                        />
-                </div>         
-                <div className="modal-password-old">
-                    <div className="row-one">Stare hasło</div>
-                        <input 
-                            className="modal-password-old-input"
-                            type="password"
-                            onChange={(event) => {
-                                setOldPassword(event.target.value);
-                            }}
-                        />
-                    {!(currentPassword===oldPassword || oldPassword==='')&&(<span class="red-star">niepoprawne hasło</span>)}
-                </div>
-                <div className="modal-password-new">
-                    <div className="row-one">Nowe hasło</div>
-                        <input 
-                            className="modal-password-input-new"
-                            type="password"
-                            onChange={(event) => {
-                                setNewPassword(event.target.value);
-                            }}
-                        />
-                </div>
-                <div className="modal-password-new-repeat">
-                    <div className="row-one">Powtórz hasło</div>
-                        <input 
-                            className="modal-password-input-new-repeat"
-                            type="password"
-                            onChange={(event) => {
-                                setRepeatPassword(event.target.value);
-                            }}
-                        />
-                        {!(repeatPassword===newPassword||repeatPassword==='')&&(<span class="red-star">powtórz hasło</span>)}
-                </div>
-                <div className="modal-country">
-                    <div className="row-one">Kraj</div>
-                        <input 
-                        type="text"
-                        className="modal-country-input"
-                        onChange={(event) => {
-                            setNewCountry(event.target.value);
-                        }}
-                        />
-                </div>
-                <div className="modal-avatar">
-                    <div className="modal-avatar-text"></div>
-                    <div className="arrow modal-left-arrow" onClick={() => previousAvatar()}>&lt;</div>
-                    <div>
-                        <img className="modal-avatar-image" src={`./images/${temporaryAvatar}.png`} alt="Awatar użytkownika" width="150" height="150" />
-                    </div>
-                    <div className="arrow modal-right-arrow"  onClick={() => nextAvatar()}>&gt;</div>
-                </div>
-                <div className="modal-avatar-current">
-                  
-                    </div>
-                <div className="modal-button">
-                    <button className="modal-button-save" type="submit" onClick={() => setNewData()} >Ok</button>
-                    <button className="modal-button-cancel" type = "button" onClick={() => clearSettings()}>Anuluj</button>
-                </div>
-                </form>
-
-            </div>
-        </div>
-    </>)
+function App() {
+  return (
+    <Switch>
+      <Route path="/" component={MainMenu} exact />
+      <AuthRoute path="/login" component={Login} />
+      <AuthRoute path="/register" component={Register} />
+      <PrivateRoute path="/settings" component={Settings} />
+      <PrivateRoute path="/global-stats" component={GlobalStats} />
+      <Route path="/game-view" component={GameView} />
+      <PrivateRoute path="/multiplayer" component={LobbyMultiplayer} />
+      <PrivateRoute path="/account" component={Account} />
+      <PrivateRoute path="/multiplayer/game-lobby" component={JoinRoom} />
+      <PrivateRoute path="/multiplayer/create-room" component={CreateRoom} />
+    </Switch>
+  );
 }
 
-export default Account;
+function PrivateRoute({ component: Component, ...rest }) {
+  const [isAuth, setAuth] = useState(true);
+  const [isLoading, setLoading] = useState(true);
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000/auth/verify")
+      .then(() => {
+        setLoading(false);
+        setAuth(true);
+      })
+      .catch(() => {
+        setLoading(false);
+        setAuth(false);
+      });
+  }, []);
+  if (isLoading) return <div>loading...</div>;
+  return (
+    <Route
+      {...rest}
+      render={(props) =>
+        isAuth ? (
+          <Component {...props} />
+        ) : (
+          <Redirect
+            to={{
+              pathname: "/login",
+              state: { from: props.location },
+            }}
+          />
+        )
+      }
+    />
+  );
+}
+
+function AuthRoute({ component: Component, ...rest }) {
+  const [isAuth, setAuth] = useState(false);
+  const [isLoading, setLoading] = useState(true);
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000/auth/verify")
+      .then(() => {
+        setLoading(false);
+        setAuth(true);
+      })
+      .catch(() => {
+        setLoading(false);
+        setAuth(false);
+      });
+  }, []);
+  if (isLoading) return <div>loading...</div>;
+  return (
+    <Route
+      {...rest}
+      render={(props) =>
+        !isAuth ? (
+          <Component {...props} />
+        ) : (
+          <Redirect
+            to={{
+              pathname: "/",
+              state: { from: props.location },
+            }}
+          />
+        )
+      }
+    />
+  );
+}
+
+export default App;
