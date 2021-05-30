bc81fe55fc8b1dad3fd382dd6df8d72a6dd577dc
RKędziora <kontakt.rkedziora@gmail.com>
2021-05-30 13:12:14 +0200

    Update agent.js

--- client/src/agent/agent.js
+++ client/src/agent/agent.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "http://pasjansklondike.herokuapp.com/";
+axios.defaults.baseURL = "https://pasjansklondike.herokuapp.com/";
 axios.defaults.withCredentials = true;
 
 export default axios;
