49d6375e59004e013fc51f6b1b3abd215b0b11e2
aglebionek <aglebionek@gmail.com>
2021-05-30 09:42:30 +0200

    Stworzeniu designu aplikacji Fixes #60

--- client/src/Components/Account/Account.js
+++ client/src/Components/Account/Account.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import "./Components/Account/AccountCyberpunk.css";
+import "./AccountCyberpunk.css";
 import axios from "axios";
 
 const Account =() => {
