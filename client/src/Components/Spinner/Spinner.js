b4aacd780bcbbde13e0426d89a5095d755d7ae2e
aglebionek <aglebionek@gmail.com>
2021-05-31 14:34:52 +0200

    spinner design

--- client/src/Components/Spinner/Spinner.js
+++ client/src/Components/Spinner/Spinner.js
@@ -2,7 +2,9 @@ import React from 'react';
 import './Spinner.css';
 
 function Spinner () {
-  return (<div className="waiting-spinner"></div>)
+  return (<div className="waiting-spinner-container">
+    <div className="waiting-spinner"></div>
+  </div>)
 }
 
 export default Spinner;
