16a1671b276a105e8a169e418a22f8ef4112c26a
aglebionek <aglebionek@gmail.com>
2021-05-31 12:22:50 +0200

    d

--- client/src/index.js
+++ client/src/index.js
@@ -4,11 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import Settings from './Components/Settings/index'
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Settings />
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
