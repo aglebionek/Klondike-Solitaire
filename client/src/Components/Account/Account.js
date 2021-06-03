a931881555149703598866dee3d930fe6e2cef86
aglebionek <aglebionek@gmail.com>
2021-06-03 13:33:13 +0200

    account design changes

--- client/src/Components/Account/Account.js
+++ client/src/Components/Account/Account.js
@@ -4,6 +4,7 @@ import Select from 'react-select'
 import ReactCountryFlag from "react-country-flag"
 import dataCountry from './country-list.json';
 import buttonClickSound from '../../soundtrack/SoundDesign/menu_click.mp3';
+import buttonHoverSound from "../../soundtrack/SoundDesign/menu_hover.mp3";
 import agent from '../../agent/agent.js';
 
 const Account =({effect}) => {
@@ -79,14 +80,57 @@ const Account =({effect}) => {
         }
 
     const selstyle ={
-            control: styles=> ({...styles, width: '225px'})
+            menu: (menu_styles, state)=> ({...menu_styles, 
+                background: 'black', 
+                border: 'solid 1px rgba(0, 214, 252, 0.6)',
+                
+            }),
+            option: (menuList_styles, state)=> ({...menuList_styles,
+                color: 'rgba(0, 214, 252, 0.9)',
+                background: 'black',
+                textShadow: state.isFocused ? '0px 0px 10px rgba(0, 214, 252, 1), 0px 0px 10px rgba(0, 214, 252, 1)' : 'none',
+                "&:hover": {
+                    background: 'black',
+                    textShadow: '0px 0px 10px rgba(0, 214, 252, 1), 0px 0px 10px rgba(0, 214, 252, 1)'
+                }
+            }),
+            singleValue: (singleValue_styles)=> ({...singleValue_styles,
+                color: 'rgba(0, 214, 252, 0.9)'
+            }),
+            control: (control_styles, state)=> ({...control_styles,
+                width: '203px', 
+                background: 'none', 
+                border: 'solid 1px rgba(0, 214, 252, 0.6)',
+                border: state.isFocused ? 'solid 1px rgba(0, 214, 252, 0.6)' : 'solid 1px rgba(0, 214, 252, 0.6)',
+                boxShadow: state.isFocused ? '0px 0px 10px rgba(0, 214, 252, 1)' : 'none',
+                "&:hover": {
+                    border: 'solid 1px rgba(0, 214, 252, 0.6)',
+                    boxShadow: '0px 0px 10px rgba(0, 214, 252, 1)'
+                },
+            }),
+            dropdownIndicator: (dropdown_styles, state)=> ({...dropdown_styles, 
+                color: 'inherit',
+                "&:hover": {
+                    color: 'inherit',
+                    textShadowColor: 'rgba(0, 214, 252, 1)',
+                    textShadowRadius: 10
+                }
+            }),
+            indicatorSeparator: (indicator_styles, state)=> ({...indicator_styles, background: 'rgba(0, 214, 252, 0.6)'}),
+            placeholder: (placeholder_styles, state)=> ({...placeholder_styles, color: 'rgba(0, 214, 252, 0.9)'}),
         };
 
     const buttonSound = () => {
             let beep = new Audio(buttonClickSound);
             beep.volume=(effect/100);
             beep.play();   
-    }     
+        };
+    
+    const buttonHover = () => {
+        let beep = new Audio(buttonHoverSound);
+        beep.volume = 1;
+        beep.play();
+      };
 
     useEffect(() => {
         agent.get(`account/${userId}`).then(({ data }) => {
@@ -115,9 +159,12 @@ const Account =({effect}) => {
 
     return (<>
         <div className = "profile-container">
-            <a className="stats__back" href="./..">
+            <div className="profile-menu-button-div">
+            <a className="account__back" href="./.." onMouseDown={buttonSound}
+            onMouseOver={buttonHover}>
                 &#129044;
             </a>
+            </div>
             <div className="profile-heading">KONTO</div>
             <div className="profile-header">
                 <div className="profile-avatar">
@@ -238,7 +285,6 @@ const Account =({effect}) => {
                     <div className="modal-country-text row-one">Kraj</div>
                     <div className="modal-country-input">
                         <Select options={options} value={value} styles={selstyle} onChange={changeHandler} />
-                        
                     </div>
                 </div>
                 <div className="modal-avatar">
