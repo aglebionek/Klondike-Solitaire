7b63805e29cf3a7a8a7d1523eb3b2c1db16da81e
aglebionek <aglebionek@gmail.com>
2021-06-05 19:08:53 +0200

    gameview changes

--- client/src/Components/GameView/GameView.js
+++ client/src/Components/GameView/GameView.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import styles from "./GameView.module.css";
+import styles from "./GameViewCyberpunk.module.css";
 import CustomDragLayer from "./CustomDrag/Custom";
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
