f752c8bf979f844d628d8c1b4c94648d36852a25
aglebionek <aglebionek@gmail.com>
2021-06-12 17:54:11 +0200

    stats table final fixes

--- client/src/Components/GlobalStats/Posts.js
+++ client/src/Components/GlobalStats/Posts.js
@@ -9,11 +9,11 @@ const Posts = ({statsList, loading, userID}) => {
         <Spinner></Spinner>
     );
 
-    return <table width="100%" id={'stats-table'}>
+    return <table width="100%" id={'stats-table'} cellSpacing="0" cellPadding="5">
         <tbody>
         {statsList.map(statsList => (
             <tr className={statsList.ID === userID ? "loggedPlayer" : ""} key={statsList.ID}>
-                <td width="40%" align="left">{statsList.ID} <img src={statsList.Avatar} width="50" height="50"/> {statsList.Nazwa} </td>
+                <td width="40%" align="left" className="posts__player"><p className="posts__id">{statsList.ID}</p> <img src={statsList.Avatar} width="50" height="50"/> <p className="posts__name">{statsList.Nazwa}</p> </td>
                 <td width="30%" align="center">{statsList.Ranking}</td>
                 <td width="30%" align="center">{statsList.Wygrane}/{statsList.Remisy}/{statsList.Przegrane}</td>
             </tr>
